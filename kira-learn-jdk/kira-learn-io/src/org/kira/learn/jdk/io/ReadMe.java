eab55dd413fa42732576ff2e515ff814f9ae73bb
zhangchaoqing <zzccqq2016@126.com>
2022-11-02 19:57:37 +0800

    add kira-learn-docker module

--- kira-learn-jdk/kira-learn-io/src/org/kira/learn/jdk/io/ReadMe.java
+++ kira-learn-jdk/kira-learn-io/src/org/kira/learn/jdk/io/ReadMe.java
@@ -8,7 +8,10 @@ public class ReadMe {
 
 
     public static void main(String[] args) {
-
+        System.out.println(System.getProperty("java.version"));
+        System.out.println(System.getenv("JAVA_HOME"));
+        System.out.println(System.getProperty("compiler.java"));
+        System.out.println(System.getProperty("java.specification.version"));
     }
 
 }
